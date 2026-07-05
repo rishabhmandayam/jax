4541a2388a6bcea3c0840a62ad058cc9e1d6e321
Rishabh Mandayam <Rishabh.Mandayam@gmail.com>
2026-07-05 19:18:22 -0400

    Update mlp.py

--- lecture03/mlp.py
+++ lecture03/mlp.py
@@ -25,33 +25,54 @@ import einops
 
 # # #
 # MODEL CODE
+@jax.tree_util.register_dataclass
+@dataclasses.dataclass
+class Model:
+    W1: Float[Array, "2 hidden"]
+    b1: Float[Array, "hidden"]
+    W2: Float[Array, "hidden 1"]
+    b2: Float[Array, "1"]
 
-type Model = tuple[
-    Float[Array, "2 hidden"],
-    Float[Array, "hidden"],
-    Float[Array, "hidden 1"],
-    Float[Array, "1"]
-]
+    @staticmethod
+    def init(
+        key:PRNGKeyArray,
+        num_hidden: int,
+    )->Self:
+        key_W1, key = jax.random.split(key)
+        W1 = jax.random.normal(
+            key=key_W1,
+            shape = (2, num_hidden),
+        )
+        b1 = jnp.zeros(num_hidden)
 
-def forward(
-    w: Model,
-    x:Float[Array, "batch_size 2"]
-) -> Float[Array, "batch_size"]:
-    W1 , b1, W2, b2 = w
-    #layer 1
-    x = x @ W1 + b1
-    #activation
-    x = jax.nn.relu(x)
-    #layer 2
-    logits = x @ W2 + b2
-    return logits
+        key_W2, key = jax.random.split(key)
+        W2 = jax.random.normal(
+            key=key_W2,
+            shape = (num_hidden, 1),
+        )
+        b2 = jnp.zeros(1)
+
+        w = Model(W1=W1, b1=b1, W2=W2, b2=b2)
+        return w
+
+    def forward(
+        self: Self,
+        x:Float[Array, "batch_size 2"]
+    ) -> Float[Array, "batch_size"]:
+        #layer 1
+        x = x @ self.W1 + self.b1
+        #activation
+        x = jax.nn.relu(x)
+        #layer 2
+        logits = x @ self.W2 + self.b2
+        return logits
 
 def loss(
     w: Model,
     xs: Float[Array, "batch_size 2"],
     ys: Bool[Array, "batch_size"],
 )->float:
-    logits = forward(w, xs)[:0]
+    logits = w.forward(xs)[:, 0]
     cross_entropies = jnp.logaddexp(0, logits) - ys * logits
     return jnp.mean(cross_entropies)
 
@@ -96,24 +117,8 @@ def main(
     print(vis_data(xs,ys))
 
     # initialize the model
-
     key_model_init, key = jax.random.split(key)
-    key_W1, key_model_init = jax.random.split(key_model_init)
-    W1 = jax.random.normal(
-        key=key_W1,
-        shape = (2, num_hidden),
-    )
-    b1 = jnp.zeros(num_hidden)
-
-    key_W2, key_model_init = jax.random.split(key_model_init)
-    W2 = jax.random.normal(
-        key=key_W2,
-        shape = (num_hidden, 1),
-    )
-    b2 = jnp.zeros(1)
-
-    w = (W1, b1, W2, b2)
-
+    w = Model.init(key = key_model_init, num_hidden=num_hidden)
     print(vis_model(w, xs, ys, -1))
 
     key_train, key = jax.random.split(key)
@@ -176,15 +181,15 @@ def vis_model(
     step: int,
 ) -> mp.plot:
     # compute predictions
-    ys_pred = jax.nn.sigmoid(forward(w, xs)[:, 0])
-    # ys_pred = jax.nn.sigmoid(w.forward(xs)[:, 0])
+    #ys_pred = jax.nn.sigmoid(forward(w, xs)[:, 0])
+    ys_pred = jax.nn.sigmoid(w.forward(xs)[:, 0])
 
     # plot
     return mp.axes(
         mp.dstack2(
             mp.function2(
-                F=lambda xs: jax.nn.sigmoid(forward(w, xs)[:,0]),
-                # F=lambda xs: jax.nn.sigmoid(w.forward(xs)[:,0]),
+                #F=lambda xs: jax.nn.sigmoid(forward(w, xs)[:,0]),
+                F=lambda xs: jax.nn.sigmoid(w.forward(xs)[:,0]),
                 xrange=(-3,3),
                 yrange=(-3,3),
                 width=40,
